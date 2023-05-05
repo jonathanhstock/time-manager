760768526658489606465ca7e5bbe88e08fd596c
Matthew Davenport <davenmport@gmail.com>
2023-05-04 22:17:58 -0700

    html templates
    Added html templates for login and schedule input. linked to express to send to browser

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
-const app = express();
-const port = 3001;
+var app = express();
+const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+//login
+app.get('/', (req, res) => { 
+  res.sendFile(__dirname + '/index.html'); 
 });
+
+//input schedule
+app.get('/schedule', (req,res) => {
+  res.sendFile(__dirname + '/schedule.html');
+})
+
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
